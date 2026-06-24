05f4cdb39de7fecc254a87e04c1ee2780cbde351
d546-star <dumso546@gmail.com>
2026-06-24 20:19:11 +0200

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -3,24 +3,34 @@ const path = require("path");
 
 const base = "https://d546-star.github.io/bursaries-alerts-sa";
 
-const files = fs.readdirSync(
-  path.join(__dirname, "public", "bursaries")
-);
+const files = [];
+
+function walk(dir, urlPath) {
+  fs.readdirSync(dir).forEach(file => {
+    const full = path.join(dir, file);
+    const stat = fs.statSync(full);
+
+    if (stat.isDirectory()) {
+      walk(full, urlPath + "/" + file);
+    } else {
+      files.push(urlPath + "/" + file);
+    }
+  });
+}
+
+walk(path.join(__dirname, "public"), "");
 
 let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
-// homepage
-xml += `<url><loc>${base}/</loc></url>\n`;
-
 files.forEach(f => {
-  xml += `<url><loc>${base}/bursaries/${f}</loc></url>\n`;
+  xml += `<url><loc>${base}${f}</loc></url>\n`;
 });
 
 xml += `</urlset>`;
 
 fs.writeFileSync(
-  path.join(__dirname, "..", "public", "sitemap.xml"),
+  path.join(__dirname, "public", "sitemap.xml"),
   xml
 );
 
-console.log("Sitemap built");
+console.log("V3 sitemap generated");
