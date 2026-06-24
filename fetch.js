6540b0c0e05dbefd92468d6a109a0ea2e4d01a99
d546-star <dumso546@gmail.com>
2026-06-24 20:29:37 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,18 +1,11 @@
-const fs = require('fs');
-
-const bursaries = [
+[
   {
-    title: "NSFAS Funding",
-    description: "Government student funding",
-    link: "https://www.nsfas.org.za"
-  },
-  {
-    title: "Capitec Bursary",
-    description: "Bank bursary programme",
-    link: "https://www.capitecbank.co.za"
+    "name": "NSFAS Bursary 2026",
+    "slug": "nsfas-bursary-2026",
+    "description": "Government funding for South African students",
+    "closingDate": "2026-09-30",
+    "provider": "NSFAS",
+    "category": "government-bursaries",
+    "keywords": ["nsfas", "funding", "south africa students"]
   }
-];
-
-fs.writeFileSync('bursaries.json', JSON.stringify(bursaries, null, 2));
-
-console.log("Updated bursaries.json");
+]
